f7050d8b85f27568956293edb69bb399f07a773c
anwesh-23 <anweshdash295@gmail.com>
2026-03-02 15:15:58 +0530

    Problem_10

--- src/HTB.java
+++ src/HTB.java
@@ -2,20 +2,41 @@ import java.util.*;
 
 public class HTB {
 
-    public static void twoSum(int[] arr, int target){
-        Map<Integer,Integer> map = new HashMap<>();
+    private LinkedHashMap<String,String> L1 =
+            new LinkedHashMap<>(5,0.75f,true){
+                protected boolean removeEldestEntry(Map.Entry e){
+                    return size()>3;
+                }
+            };
 
-        for(int num: arr){
-            int comp = target - num;
-            if(map.containsKey(comp)){
-                System.out.println("Pair: "+num+" + "+comp);
-            }
-            map.put(num,1);
+    private Map<String,String> L2 = new HashMap<>();
+
+    public HTB(){
+        L2.put("video1","SSD");
+        L2.put("video2","SSD");
+    }
+
+    public void getVideo(String id){
+
+        if(L1.containsKey(id)){
+            System.out.println("L1 HIT");
+            return;
         }
+
+        if(L2.containsKey(id)){
+            System.out.println("L2 HIT → promoted to L1");
+            L1.put(id,"MEM");
+            return;
+        }
+
+        System.out.println("DB HIT → added to L2");
+        L2.put(id,"SSD");
     }
 
     public static void main(String[] args){
-        int[] tx = {500,300,200,700};
-        twoSum(tx,500);
+        HTB cache = new HTB();
+        cache.getVideo("video1");
+        cache.getVideo("video1");
+        cache.getVideo("video3");
     }
 }
